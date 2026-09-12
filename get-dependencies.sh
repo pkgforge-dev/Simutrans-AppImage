84184972b69d79e2fd2e385724375ab86c0444bb
Link4Electronics <link4electronics@gmail.com>
2026-09-12 01:50:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,9 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    simutrans        \
-    simutrans-pak128
+pacman -Syu --noconfirm simutrans simutrans-pak128
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
